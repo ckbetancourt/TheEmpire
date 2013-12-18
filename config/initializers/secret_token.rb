3814d83984e0ac5d14a79586a4304d047878246c
ckbetancourt <chriskbetancourt@gmail.com>
2013-12-18 14:39:47 -0500

    Bootstrap editable spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheEmpire::Application.config.secret_key_base = 'd662f76c3c1cd0a9d986a8efa43fc95b6d38e180ae1ddff15b971836420daeb2285cbf04798cd885175cbc40fe22388f8312d8912fe0d0c8d2aad4a46ebaa93d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TheEmpire::Application.config.session_store :cookie_store, key: '_TheEmpire_session'
